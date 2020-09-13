fd9b75e53ca08079c5bad7071c95c6ab534808e1
Neil Rodriguez <56770257+Tenzorialpuma@users.noreply.github.com>
2020-09-13 13:17:18 -0500

    Add files via upload

---
+++ Tenzorialzoom.bash
@@ -0,0 +1,4 @@
+  xdg-open zoommtg://zoom.us/join?confno=(meeting ID of a recurring meeting)&pwd=(password of a recurring meeting)&zc=0&browser=chrome&uname=(the name you want to have in the zoom meeting)
+sleep 10
+xdotool type "password of the recurring meeting THIS NEEDS TO BE IN QUOTES"
+xdotool key KP_Enter
